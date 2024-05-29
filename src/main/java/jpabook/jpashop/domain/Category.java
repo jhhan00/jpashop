b9e01c3ce4978ef8f72f6ffc77a5aad919f2c504
jhhan00 <jhhan009@gmail.com>
2024-05-29 23:12:04 +0900

    엔티티 개발 시 필요한 사항들 반영

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -29,10 +29,16 @@ public class Category {
     private List<Item> items = new ArrayList<>();
 
     // === SELF JOIN ===
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //연관관계 메서드
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
