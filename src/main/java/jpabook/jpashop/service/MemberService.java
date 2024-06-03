b62ac4fcac73026da18ae6a5f3d3e76e995702cb
jhhan00 <jhhan009@gmail.com>
2024-06-03 22:32:29 +0900

    Lombok 활용 - 어노테이션으로 autowired 해결

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,14 +11,10 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
-
-    @Autowired
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     /**
      * 회원 가입
