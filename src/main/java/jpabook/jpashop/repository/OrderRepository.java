898de7212cc4273af777218dbc9ce8ee60ae1d69
jhhan00 <jhhan009@gmail.com>
2024-06-09 22:19:02 +0900

    주문 찾기 - 동적쿼리 활용2(JPA Criteria 사용)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,11 +2,13 @@ package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -26,7 +28,6 @@ public class OrderRepository {
     /**
      * 주문 1개 id로 찾기
      * @param id 주문 ID
-     * @return
      */
     public Order findOne(Long id) {
         return em.find(Order.class, id);
@@ -35,8 +36,9 @@ public class OrderRepository {
     /**
      * 필터 조건에 맞는 주문 모두 찾기
      * status, name 이 안 들어있을 수도 있으니 동적 쿼리가 되어야 함
+     * String을 만들어 주는 것이어서 되게 복잡함
      */
-    public List<Order> findAll(OrderSearch orderSearch) {
+    public List<Order> findAllByString(OrderSearch orderSearch) {
         String jpql = "select o from Order o join o.member m";
         boolean isFirstCondition = true;
 
@@ -71,9 +73,32 @@ public class OrderRepository {
         }
 
         return query.getResultList();
+    }
 
-//        return em.createQuery(jpql, Order.class)
-//                .setMaxResults(1000)        //최대 1000건 조회
-//                .getResultList();
+    /**
+     * JPA Criteria 를 이용해서 동적쿼리 진행
+     * String 더하는 것보다는 낫지만 그래도 굉장히 복잡함
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+        if(orderSearch.getOrderStatus() != null) {
+            //주문 상태 검색
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        if(StringUtils.hasText(orderSearch.getMemberName())) {
+            //회원 이름 검색
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
     }
 }
