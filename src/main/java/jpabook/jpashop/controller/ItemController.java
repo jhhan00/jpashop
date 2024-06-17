e871970ede80eb20c0edee6e71829350ddfdee91
jhhan00 <jhhan009@gmail.com>
2024-06-17 22:20:35 +0900

    상품 수정을 할 수 있도록 코드 추가

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -41,5 +43,35 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemInfo(@PathVariable("itemId") Long itemId, Model model) {
+        Book book1 = (Book) itemService.findOne(itemId);
+
+        BookForm bf = new BookForm();
+        bf.setId(book1.getId());
+        bf.setName(book1.getName());
+        bf.setPrice(book1.getPrice());
+        bf.setStockQuantity(book1.getStockQuantity());
+        bf.setAuthor(book1.getAuthor());
+        bf.setIsbn(book1.getIsbn());
+
+        model.addAttribute("form", bf);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@PathVariable("itemId") Long itemId, @ModelAttribute("form") BookForm form) {
+        Book book1 = new Book();
+        book1.setId(form.getId());
+        book1.setName(form.getName());
+        book1.setPrice(form.getPrice());
+        book1.setStockQuantity(form.getStockQuantity());
+        book1.setAuthor(form.getAuthor());
+        book1.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book1);
+        return "redirect:/items";
     }
 }
